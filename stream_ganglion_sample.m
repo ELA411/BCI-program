39e2787f6f5238eac5d7860200b871af94434263
Pontus Svensson <pontus.github@doughnut.anonaddy.com>
2023-11-30 11:20:51 +0100

    method

--- stream_ganglion_sample.m
+++ stream_ganglion_sample.m
@@ -1,26 +1,56 @@
-% BoardShim class to communicate with a board 
+% BoardShim class to communicate with a board
+BoardShim.set_log_file('brainflow.log');
+BoardShim.enable_dev_board_logger();
+
 % Create BoardShim object
 params = BrainFlowInputParams();
 % Change to this when using real hardware BoardIds.GANGLION_NATIVE_BOARD
-board_shim = BoardShim(int32(BoardIds.SYNTHETIC_BOARD), params);
+board_shim = BoardShim(int32(BoardIds.GANGLION_NATIVE_BOARD), params);
 preset = int32(BrainFlowPresets.DEFAULT_PRESET);
-
+% Time stamp channel
+board_shim.get_timestamp_channel(int32(BoardIds.SYNTHETIC_BOARD), preset);
 % prepare BrainFlow’s streaming session, allocate required resources
 board_shim.prepare_session();
 
-% add streamer 
+% add streamer
 board_shim.add_streamer('file://data_default.csv:w', preset);
 
-% start streaming thread, store data in internal ringbuffer 
+% start streaming thread, store data in internal ringbuffer
 board_shim.start_stream(45000, '');
-pause(5);
 
-% stop streaming thread, doesnt release other resources
-board_shim.stop_stream();
+% Create a figure for plotting
+figure;
+hold on;
 
-% get latest datapoints, doesnt remove it from internal buffer
-data = board_shim.get_current_board_data(10, preset);
-disp(data);
+% Continuously acquire and plot data
+figure; % Create a new figure for plotting
+try
+    while true
+        %pause(0.01); 
 
-% release session
-board_shim.release_session();
+        % Get the latest datapoints (4x10 matrix)
+        data = board_shim.get_current_board_data(1000, preset);
+
+        % Clear the current figure
+        clf;
+
+        % Plot first 4 channels
+        for i = 1:4 % Iterate through each channel
+            subplot(4, 1, i); % Create a subplot for each channel
+            plot(data(i, :)); % Plot the data of the current channel
+            title(['Channel ' num2str(i)]); % Add a title to each subplot
+        end
+
+        drawnow; % Update the plots
+
+    end
+catch ME
+    disp('Error or interruption occurred. Stopping data acquisition...');
+end
+
+% After exiting the loop
+board_shim.stop_stream(); % Stop streaming
+board_shim.release_session(); % Release session
+close; % Close the figure
+
+
