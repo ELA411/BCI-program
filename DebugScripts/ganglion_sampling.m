7fe3a6bb7a843912a1296cf6c6e2043ba185b0e6
Pontus Svensson <github@doughnut.mailer.me>
2023-12-21 13:12:25 +0100

    Sliding window implementation

--- DebugScripts/ganglion_sampling.m
+++ DebugScripts/ganglion_sampling.m
@@ -40,7 +40,7 @@ board_shim.prepare_session();
 % add streamer
 % ---------------------------------------------------------------------
 currentDateTime = datetime('now','Format', 'yyyy-MM-dd_HHmmss'); % Format as 'YYYYMMDD_HHMMSS'
-fileName = ['file://Datasets/Brainflow/Brainflow_',session,'_EEG_',char(currentDateTime),'.txt:w'];
+fileName = 'file://Brainflow_EEG_.txt:w';
 board_shim.add_streamer(fileName, preset);
 
 % ---------------------------------------------------------------------
@@ -50,6 +50,7 @@ bufferSize = 10000;
 board_shim.start_stream(bufferSize, '');
 eegBuffer = []; % Buffer to store temporary values for data quality calculation
 eegBufferProcessing = [];
+overlapSamples = round(0.025 * 200);
 % ---------------------------------------------------------------------
 % Package integrity
 % ---------------------------------------------------------------------
@@ -59,49 +60,49 @@ oldData = zeros(15,25);
 % ---------------------------------------------------------------------
 % Main loop
 sameData = 0;
+prevVoltage = [];
+col = 1;
+firstIteration = true;
+threshold = 50;
+overSampling = false;
 % ---------------------------------------------------------------------
 slidingWindow = tic;
 while true
-    % ---------------------------------------------------------------------
-    % Collect data
-    % ---------------------------------------------------------------------
     dataInBuffer = board_shim.get_board_data_count(preset); % Check how many samples are in the buffer
     if dataInBuffer > 0
+        data = board_shim.get_board_data(1, preset); % Take available packages and remove them from buffer
         samples = samples + 1;
-        data = board_shim.get_board_data(1, preset);
-        % send(EEG_main_queue, ['Samples in buffer: ', dataInBuffer]);
-        % send(EEG_main_queue, "Data read from ganglion");
-        % ---------------------------------------------------------------------
-        % Iterate through all the packages received
-        % ---------------------------------------------------------------------
-        % for col = 1:size(data,2)
-        packageid = data(1,1);
-        channel1 = data(2,1);
-        channel2 = data(3,1);
-        channel3 = data(4,1);
-        channel4 = data(5,1);
-        timestamp = data(14,1);
+        packageid = data(1,col);
+        channel1 = data(2,col);
+        channel2 = data(3,col);
+        channel3 = data(4,col);
+        channel4 = data(5,col);
+        timestamp = data(14,col);
 
-        eegBuffer = [eegBuffer; channel1, channel2, channel3, channel4, packageid, timestamp];
-        eegBufferProcessing = [eegBufferProcessing; channel1, channel2, channel3, channel4];
-
-        % ---------------------------------------------------------------------
-        % Check so that the packages are in order
-        % ---------------------------------------------------------------------
-
-        % ---------------------------------------------------------------------
-        % Increase samples, save the timestamp, and store values in temporary qualityBuffer
-        % ---------------------------------------------------------------------
-        if toc(slidingWindow)>=0.25
-            % send(EEG_processing_queue, eegBufferProcessing);
-            % send(EEG_save_queue, eegBuffer);
-            % send(EEG_main_queue, ['Number of samples for 250 ms ', size(eegBufferProcessing, 1)]);
-            disp(size(eegBufferProcessing, 1));
-            eegBuffer = [];
-            eegBufferProcessing = [];
-            slidingWindow = tic;
+        % eegBuffer = [eegBuffer; channel1, channel2, channel3, channel4, packageid, timestamp];
+        if overSampling
+            eegBufferProcessing = [prevVoltage; eegBufferProcessing; channel1, channel2, channel3, channel4, timestamp];
+            overSampling = false;
+        else
+            eegBufferProcessing = [eegBufferProcessing; channel1, channel2, channel3, channel4, timestamp];
         end
-        % end
 
+        if samples >= threshold
+            % Retain the last 25 ms of data in eegBufferProcessing for overlap
+            if size(eegBufferProcessing, 1) > overlapSamples
+                prevVoltage = eegBufferProcessing(end-overlapSamples+1:end, :);
+                overSampling = true;
+            else
+                prevVoltage = [];
+            end
+            % eegBuffer = [];
+            eegBufferProcessing = [];
+            % slidingWindow = tic;
+            samples = 0;
+            if firstIteration
+                threshold = 45;
+                firstIteration = false;
+            end
+        end
     end
 end
--- DebugScripts/myDaqTest.m
+++ DebugScripts/myDaqTest.m
@@ -1,3 +1,4 @@
+clear, clc;
 d = daq("ni");
 d.Rate = 1000;
 ch = addinput(d,"myDAQ1",0:1,"Voltage");
@@ -5,9 +6,29 @@ start(d, "Continuous");
 voltage = [];
 starttime = tic();
 runtime = tic();
-while toc(runtime)<=9
-    [scanData, timeStamp] = read(d, seconds(1),"Outputformat", "Matrix");
-    voltage = [voltage; scanData, timeStamp];
+overlapSamples = round(0.025 * 1000); % Replace Fs with your actual sampling rate
+prevVoltage = []; % Initialize an array to store the overlapping data
+firstIteration = true;
+% Really important to start in continuous mode
+while true
+    % Read data
+    if firstIteration
+        [scanData, timeStamp] = read(d, seconds(0.25), "OutputFormat","Matrix");
+        firstIteration = false;
+    else
+        [scanData, timeStamp] = read(d, seconds(0.225), "OutputFormat","Matrix");
+    end
+
+    % Append the previous overlap to the current data
+    voltage = [prevVoltage; scanData(:,1), scanData(:,2), timeStamp]; % Append overlap
+    voltage_save = [scanData(:,1), scanData(:,2), timeStamp]; % Combine with timestamps for saving
+
+    % Store last 25 ms of data for the next overlap
+    if size(scanData, 1) > overlapSamples
+        prevVoltage = [scanData(end-overlapSamples+1:end, 1:2), timeStamp(end-overlapSamples+1:end)];
+    else
+        prevVoltage = [];
+    end
 end
 endtime = toc(starttime);
 fprintf("Sample rate: %d\n", int64(1/(endtime/size(voltage,1))));
