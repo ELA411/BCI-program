b445b861ff3d8a03c87f89030bd5f2d15c5e7e58
Pontus Svensson <github@doughnut.mailer.me>
2024-01-10 13:59:17 +0100

    License and description

--- Recording/EMG_recording.m
+++ Recording/EMG_recording.m
@@ -1,3 +1,31 @@
+% Script Name: EMG_recording.m
+% Author: Pontus Svensson
+% Date: 2024-01-10
+% Version: 1.0.0
+% ---------------------------------------------------------------------
+% Description: This script is the protocol used for recording EMG dataset
+% ---------------------------------------------------------------------
+% MIT License
+% Copyright (c) 2024 Pontus Svensson
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+%
 clear, clc;
 name = 'Pontus';
 setting = 'Ch_1_longitude_Ch_2_transverse';
